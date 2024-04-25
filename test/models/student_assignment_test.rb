e2af635704e42b59cdf9c61e62205acb0c278287
jaketothepast <windlejacob12@gmail.com>
2024-04-25 00:27:55 -0400

    Saving cheat_score tests

--- test/models/student_assignment_test.rb
+++ test/models/student_assignment_test.rb
@@ -9,5 +9,13 @@ class StudentAssignmentTest < ActiveSupport::TestCase
     sa = student_assignments(:one)
     sa.cheat_score = -1
     assert_equal false, sa.save
+
+    sa.cheat_score = 101
+    assert_equal false, sa.save
+
+    (1..100).each do |score|
+      sa.cheat_score = score
+      assert sa.save
+    end
   end
 end
