6d12459167f6776491c4683ad832ab61adaf1e21
jaketothepast <windlejacob12@gmail.com>
2024-04-24 22:44:36 -0400

    Adds assignments to a table, test for getting rows

--- test/controllers/assignments_controller_test.rb
+++ test/controllers/assignments_controller_test.rb
@@ -4,12 +4,18 @@ class AssignmentsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get assignments_index_url
     assert_response :success
-    assert_select 'div', 'MyString'
+    assert_select 'td', 'MyString'
   end
 
   test "should get show" do
     assignment = assignments(:one)
     get assignments_show_url(assignment.id)
     assert_response :success
+  end
+
+  test "index has num_fixtures assignments" do
+    count = Assignment.all.count
+    get assignments_index_url
+    assert_select 'td', count * 2 # Number of fields in a row.
   end
 end
